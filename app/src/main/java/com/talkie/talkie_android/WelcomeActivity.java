09c373aa45bfba3239b1cfe2e8e6ea53d429bb28
mszewczyk <szewczykm94@gmail.com>
2017-04-15 17:14:56 +0200

    Created logic for moving to another activity

--- app/src/main/java/com/talkie/talkie_android/WelcomeActivity.java
+++ app/src/main/java/com/talkie/talkie_android/WelcomeActivity.java
@@ -1,13 +1,36 @@
 package com.talkie.talkie_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 
 public class WelcomeActivity extends AppCompatActivity {
+    Button fingerprintLoginButton;
+    Button normalLoginButton;
+
+    private void initElements(){
+        fingerprintLoginButton = (Button) findViewById(R.id.fingerprint_login_button);
+        normalLoginButton = (Button) findViewById(R.id.normal_login_button);
+    }
+
+    private void setListeners(){
+        fingerprintLoginButton.setOnClickListener((a) -> {
+
+        });
+
+        normalLoginButton.setOnClickListener((a) -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initElements();
+        setListeners();
     }
 }
