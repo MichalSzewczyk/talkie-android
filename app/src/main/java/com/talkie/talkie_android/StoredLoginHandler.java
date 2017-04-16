03c905bdfd5a716b8bcc776bd2e41d48243f24c7
mszewczyk <szewczykm94@gmail.com>
2017-04-16 10:32:29 +0200

    Applied encrypting logic

--- app/src/main/java/com/talkie/talkie_android/StoredLoginHandler.java
+++ app/src/main/java/com/talkie/talkie_android/StoredLoginHandler.java
@@ -16,22 +16,32 @@ public class StoredLoginHandler implements LoginHandler {
     private final static String CRED_KEY = "cred";
     private final ParsingService parsingService;
     private final SharedPreferences settings;
+    private final EncryptionService encryptionService;
 
     public StoredLoginHandler(Context context) {
         parsingService = new JsonParsingFacade(new ObjectMapper(), new MessageTypeMatcher());
         this.settings = PreferenceManager.getDefaultSharedPreferences(context);
+        this.encryptionService = new EncryptionServiceImpl();
     }
 
     @Override
     public Optional<Tuple> getStoredCredentials() {
         String serializedTuple = settings.getString(CRED_KEY, "");
-        return parsingService.deserialize(serializedTuple, Tuple.class);
+        Optional<Tuple> deserialized = parsingService.deserialize(serializedTuple, Tuple.class);
+        if(!deserialized.isPresent()){
+            return deserialized;
+        }
+        String encrypted = (String) deserialized.get().getValue();
+        String decrypted = encryptionService.decrypt(encrypted);
+        Tuple result = new Tuple(deserialized.get().getKey(), decrypted);
+        return Optional.of(result);
     }
 
     @Override
     public void storeCredentials(String login, String password) {
         SharedPreferences.Editor editor = settings.edit();
-        Tuple tuple = new Tuple<>(login, password);
+        String encodedPassword = encryptionService.encode(password);
+        Tuple tuple = new Tuple<>(login, encodedPassword);
         String serializedTuple = null;
         try {
             serializedTuple = parsingService.serialize(tuple).orElseThrow(NullPointerException::new);
