900017bec57b29694861fa4a5fcbd6166111cb68
mszewczyk <szewczykm94@gmail.com>
2017-04-22 17:48:37 +0200

    Created abstract task

---
+++ app/src/main/java/com/talkie/android/rest/tasks/AbstractTask.java
@@ -0,0 +1,25 @@
+package com.talkie.android.rest.tasks;
+
+import android.os.AsyncTask;
+
+public abstract class AbstractTask<T> extends AsyncTask<Void, Void, T> {
+    protected final static String ORIGIN = "http://10.0.2.2:8090/";
+    protected final static String LOGIN_REQUEST = "            query{\n" +
+            "              login(login:\"%s\", password:\"%s\"){\n" +
+            "                name\n" +
+            "                lastName\n" +
+            "                avatar\n" +
+            "                id\n" +
+            "                friends{\n" +
+            "                    id\n" +
+            "                    name\n" +
+            "                    lastName\n" +
+            "                    avatar\n" +
+            "                    description\n" +
+            "                }\n" +
+            "                error\n" +
+            "                message\n" +
+            "              }\n" +
+            "            }";
+
+}
